abc522ea3d3965fee8bb8cf490c9c1b7ed166675
yanghd <yanghd@glodon.com>
2021-04-16 17:36:24 +0800

    yanghd hotdata

--- mallDb/src/main/java/com/bishe/db/db/MemoryService.java
+++ mallDb/src/main/java/com/bishe/db/db/MemoryService.java
@@ -14,8 +14,8 @@ import java.util.*;
 public class MemoryService {
 
     private static final int cacheSize = 1000;
-    private static HashMap<String, MemoryDb> dataMap = new HashMap<>();
-    private static LinkedHashMap<String, MemoryDb> dataMap1 = new LinkedHashMap<String, MemoryDb>(5,0.75F ,true){
+    private static HashMap<String, MemoryDb> dataMap1 = new HashMap<>();
+    private static LinkedHashMap<String, MemoryDb> dataMap = new LinkedHashMap<String, MemoryDb>(5,0.75F ,true){
         @Override
         protected boolean removeEldestEntry(Map.Entry<String, MemoryDb> var){
             if(cacheSize <= dataMap.size()){
@@ -25,12 +25,14 @@ public class MemoryService {
         }
     };
 
+    private static Set<MemoryDb> hotData = new HashSet<>();
+    private static final int hotDataSize = 5;
+
     //TODO 异步
     //TODO 检测内存状况
     //TODO LRU https://my.oschina.net/zjllovecode/blog/1634410
     public void save(String key, MemoryDb memoryDb) {
         dataMap.put(key, memoryDb);
-        System.out.println(dataMap.size());
     }
 
 
@@ -42,8 +44,41 @@ public class MemoryService {
             dataMap.remove(key);
             return null;
         }
+        if (dataMap.get(key) != null) {
+            if (dataMap.get(key).getCount() == null) {
+                dataMap.get(key).setCount(1);
+            }else {
+                dataMap.get(key).setCount(dataMap.get(key).getCount() + 1);
+            }
+        }
+
         return dataMap.get(key).getValue();
     }
 
+    private void putInHots(MemoryDb memoryDb){
+        if (hotData.size() < hotDataSize){
+            hotData.add(memoryDb);
+            return;
+        }
+        MemoryDb minCountData = hotData.iterator().next();
+
+        for (MemoryDb data : hotData) {
+            if (data.getCount() < minCountData.getCount()) {
+                minCountData = data;
+            }
+        }
+        if (minCountData.getCount()>= memoryDb.getCount()) {
+            return;
+        }
+        hotData.remove(minCountData);
+        hotData.add(memoryDb);
+
+
+    }
+
+
+//    public Map<String, String> getHotData(int size){
+//        hotData
+//    }
 
 }
--- mallDb/src/main/java/com/bishe/db/domain/MemoryDb.java
+++ mallDb/src/main/java/com/bishe/db/domain/MemoryDb.java
@@ -2,6 +2,7 @@ package com.bishe.db.domain;
 
 
 import java.util.Date;
+import java.util.Objects;
 
 
 public class MemoryDb {
@@ -12,6 +13,8 @@ public class MemoryDb {
 
     private Date failureTime;
 
+    private Integer count;
+
     public MemoryDb() {
     }
 
@@ -19,6 +22,19 @@ public class MemoryDb {
         this.key = key;
         this.value = value;
         this.failureTime = failureTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemoryDb memoryDb = (MemoryDb) o;
+        return getKey().equals(memoryDb.getKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getKey());
     }
 
     public String getKey() {
@@ -43,5 +59,13 @@ public class MemoryDb {
 
     public void setFailureTime(Date failureTime) {
         this.failureTime = failureTime;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
     }
 }
